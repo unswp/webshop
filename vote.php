8464a5ba873d181e6f0030229776de6e1528bcdc
Vadim Ponomarev <longinusjanitus@outlook.com>
2021-07-10 12:06:03 +1000

    New stable version

--- vote.php
+++
@@ -1,6 +0,0 @@
-<?php
-$entity = $_REQUEST['id'];
-$rating = file_get_contents($entity.'/rating');
-$rating = $rating + 1;
-file_put_contents($entity.'/rating', $rating);
-chmod($entity.'/rating', 0777);
--- write.php
+++
@@ -1,7 +0,0 @@
-<?php
-$name = $_POST['name'];
-$content = $_POST['content'];
-if (file_exists('noedit')) {} else {
-    file_put_contents($name, $content);
-    chmod($name, 0777);
-}
